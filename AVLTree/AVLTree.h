8ae9fb46f1a38dcc6d2dcd86e53b07981dc7229a
Kaan <Kaan_k193@hotmail.com>
2017-07-16 01:06:19 +0300

    Initial commit

---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Node {
+	Node *parent, *leftChild, *rightChild;
+	int balanceFactor;
+};
+
+class AVLTree {
+public:
+	int Search(int item);
+	void Insert(int item);
+	void Delete(int item);
+private:
+	void LLRotation(Node * node);
+	void RRRotation(Node * node);
+	void LRRotation(Node * node);
+	void RLRotation(Node * node);
+	Node root;
+};
