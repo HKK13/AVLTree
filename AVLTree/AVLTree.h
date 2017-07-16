48680708a74e32e4ac45264d4470f3d45bc3c044
Kaan <Kaan_k193@hotmail.com>
2017-07-16 15:57:30 +0300

    Add recursive height calculation to node

--- AVLTree/AVLTree.h
+++ AVLTree/AVLTree.h
@@ -2,7 +2,27 @@
 
 struct Node {
 	Node *parent, *leftChild, *rightChild;
-	int balanceFactor;
+	int leftHeight, rightHeight;
+	int nodeValue;
+
+	Node(int value) {
+		this->leftHeight = 0;
+		this->rightHeight = 0;
+		this->nodeValue = value;
+	}
+
+	int getBalanceFactor() {
+		return leftHeight - rightHeight;
+	}
+
+	int getHeight() {
+		if (leftChild)
+			leftHeight = 1 + leftChild->getHeight();
+		if (rightChild)
+			rightHeight = 1 + rightChild->getHeight();
+
+		return leftHeight < rightHeight ? rightHeight : leftHeight;
+	}
 };
 
 class AVLTree {
